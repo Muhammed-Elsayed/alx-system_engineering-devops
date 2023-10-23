59b42f7e3b63c368744bbbd1bf0a031b6f21a11f
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-10-23 18:55:17 +0300

    0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Rest api to fetch some data"""
+
+import requests
+from sys import argv
+
+
+# link to get the data
+link = "https://jsonplaceholder.typicode.com/users/"
+
+# request the data for certain user
+response = requests.get(link + argv[1])
+
+# convert data into json
+json_data = response.json()
+
+employee_name = json_data["name"]
+
+# request todos data for one user
+todos_response = requests.get(link + argv[1] + "/todos")
+
+# convert data into json
+todos_response_json = todos_response.json()
+
+
+# getting the number of tasks and the title of it
+titles_ls = []
+for task in todos_response_json:
+    if (task.get('completed') is True):
+        titles_ls.append(task.get('title'))
+
+
+print("Employee {}is done with tasks({}/{}): "
+      .format(employee_name, len(titles_ls), len(todos_response_json)))
+
+for title in titles_ls:
+    print("\t {}".format(title))
