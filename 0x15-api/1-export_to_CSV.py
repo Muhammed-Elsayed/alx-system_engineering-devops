c572444b34372f46af207908c6195b74c042fdce
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-10-24 17:26:41 +0300

    1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,41 +1,37 @@
 #!/usr/bin/python3
-"""A Python script that, using this REST API, for a given employee ID."""
+"""fetching some data using rest api using python and request library"""
+import csv
 import requests
 from sys import argv
 
 
-def display_tasks():
-    """Python script that fetches data for a given employee ID."""
-    # link to fetch data from the API
+def display():
+    """display function"""
     link = "https://jsonplaceholder.typicode.com/users/"
 
-    # get the data from
     response = requests.get(link + argv[1])
+    # get the employee name in json
+    employee_name = response.json().get("name")
+    # get all the tasks data in json format
+    todos_response_json = requests.get(link + argv[1] + "/todos").json()
 
-    # convert the data to json format
-    data = response.json()
+    file_name = "{}.csv".format(argv[1])
 
-    # get the name of the employee
-    name = data.get("name")
-    print("Employee {} is done with tasks".format(name), end="")
-
-    # get the total number of tasks of the employee
-    response = requests.get(link + argv[1] + "/todos")
-
-    # convert the data to json format
-    data = response.json()
-
-    # get the completed tasks and the total number of them
     tasks = []
-    for task in data:
-        if task.get("completed") is True:
-            tasks.append(task.get("title"))
-    print("({}/{}):".format(len(tasks), len(data)))
+    for task in todos_response_json:
+        data = [
+            argv[1],
+            employee_name,
+            task.get('completed'),
+            task.get('title')
+        ]
+        tasks.append(data)
 
-    # print the completed tasks of the employee
-    for task in tasks:
-        print("\t {}".format(task))
+    with open(file_name, "w") as new_file:
+        csv_writer = csv.writer(new_file, quoting=csv.QUOTE_ALL)
+        for value in tasks:
+            csv_writer.writerow(value)
 
 
 if __name__ == "__main__":
-    display_tasks()
+    display()
