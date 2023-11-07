58854e3536e7dba8157fc421e00fc4b1ae6f1303
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-11-07 20:33:54 +0200

    1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""retrieving the top ten hot posts title reddit"""
+"""Using requests library"""
 import requests
 
 
 def top_ten(subreddit):
-    """retreiving the top ten hot posts"""
-    url = 'https://www.reddit.com/r/{}/top.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mymachine/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """Get Top ten from reddit API"""
+    api_url = f"https://www.reddit.com/r/{subreddit}/top.json?limit=10"
+
+    headers = {"User-Agent": "MyApp/1.0"}
+
+    response = requests.get(api_url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
-        json_response = response.json()
-        for item in json_response['data']['children']:
-            print(item['data']['title'])
+        data = response.json()
+        for post in data["data"]["children"]:
+            print(post["data"]["title"])
     else:
         print(None)
