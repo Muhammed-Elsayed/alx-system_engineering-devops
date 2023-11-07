93bbabd211026476c6c1b992d3b0c8f082df927c
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-11-07 20:28:35 +0200

    1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""retrieving the top ten hot posts title reddit"""
+import requests
+
+
+def top_ten(subreddit):
+    """retreiving the top ten hot posts"""
+    url = 'https://www.reddit.com/r/{}/top.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mymachine/1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        json_response = response.json()
+        for item in json_response['data']['children']:
+            print(item['data']['title'])
+    else:
+        print(None)
